cd924430fbd9b726734657c6c1022e39f125a665
manish <maniish3102@gmail.com>
2026-03-07 13:01:36 +0530

    Revert "updated"
    This reverts commit 8048c3dce13f9fbc7f831286e5e735df8b77c3a5.

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                         <div class="col-md-3 tp-btn-trigger">
                             <div class="hero-banner-one__circle tp-btn-bounce">
                                 <div class="hero-banner-one__circle__logo">
-                                    <img src="img/techscalify.png" alt="Tech Scalify Logo - IT Services Company" width="150" height="150" fetchpriority="high">
+                                    <img src="img/techscalify.png" alt="Tech Scalify Logo - IT Services Company" width="150" height="150">
                                 </div>
                                 <div class="curved-circle">
                                     <div class="curved-circle--item" data-circle-text-options='{
@@ -965,42 +965,38 @@
 <!-- back-to-top-end -->
 
 
-<!-- Critical JavaScript: jQuery must load first (synchronous) -->
 <script src="assets/vendors/jquery/jquery-3.7.0.min.js"></script>
+<script src="assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
+<script src="assets/vendors/jarallax/jarallax.min.js"></script>
+<script src="assets/vendors/jquery-ui/jquery-ui.js"></script>
+<script src="assets/vendors/date-time-picker/jquery.datetimepicker.min.js"></script>
+<script src="assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
+<script src="assets/vendors/jquery-appear/jquery.appear.min.js"></script>
+<script src="assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js"></script>
+<script src="assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="assets/vendors/jquery-validate/jquery.validate.min.js"></script>
+<script src="assets/vendors/nouislider/nouislider.min.js"></script>
+<script src="assets/vendors/tiny-slider/tiny-slider.js"></script>
+<script src="assets/vendors/wnumb/wNumb.min.js"></script>
+<script src="assets/vendors/owl-carousel/js/owl.carousel.min.js"></script>
+<script src="assets/vendors/aos/aos.js"></script>
+<script src="assets/vendors/imagesloaded/imagesloaded.min.js"></script>
+<script src="assets/vendors/isotope/isotope.js"></script>
+<script src="assets/vendors/slick/slick.min.js"></script>
+<script src="assets/vendors/countdown/countdown.min.js"></script>
+<script src="assets/vendors/jquery-circleType/jquery.circleType.js"></script>
+<script src="assets/vendors/jquery-lettering/jquery.lettering.min.js"></script>
 
-<!-- Defer non-critical JavaScript to improve page load -->
-<script defer src="assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script defer src="assets/vendors/bootstrap-select/bootstrap-select.min.js"></script>
-<script defer src="assets/vendors/jarallax/jarallax.min.js"></script>
-<script defer src="assets/vendors/jquery-ui/jquery-ui.js"></script>
-<script defer src="assets/vendors/date-time-picker/jquery.datetimepicker.min.js"></script>
-<script defer src="assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
-<script defer src="assets/vendors/jquery-appear/jquery.appear.min.js"></script>
-<script defer src="assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js"></script>
-<script defer src="assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js"></script>
-<script defer src="assets/vendors/jquery-validate/jquery.validate.min.js"></script>
-<script defer src="assets/vendors/nouislider/nouislider.min.js"></script>
-<script defer src="assets/vendors/tiny-slider/tiny-slider.js"></script>
-<script defer src="assets/vendors/wnumb/wNumb.min.js"></script>
-<script defer src="assets/vendors/owl-carousel/js/owl.carousel.min.js"></script>
-<script defer src="assets/vendors/aos/aos.js"></script>
-<script defer src="assets/vendors/imagesloaded/imagesloaded.min.js"></script>
-<script defer src="assets/vendors/isotope/isotope.js"></script>
-<script defer src="assets/vendors/slick/slick.min.js"></script>
-<script defer src="assets/vendors/countdown/countdown.min.js"></script>
-<script defer src="assets/vendors/jquery-circleType/jquery.circleType.js"></script>
-<script defer src="assets/vendors/jquery-lettering/jquery.lettering.min.js"></script>
-
-<!-- GSAP: Load with defer for better performance -->
-<script defer src="assets/vendors/gsap/gsap.js"></script>
-<script defer src="assets/vendors/gsap/scroll-smoother.js"></script>
-<script defer src="assets/vendors/gsap/scroll-trigger.js"></script>
-<script defer src="assets/vendors/gsap/scroll-to-plugin.js"></script>
-<script defer src="assets/vendors/gsap/split-text.js"></script>
-<script defer src="assets/vendors/gsap/gsap-lotech.js"></script>
-
-<!-- Template JS: Must load last, after all dependencies -->
-<script defer src="assets/js/lotech.js"></script>
+<!-- gsap js -->
+<script src="assets/vendors/gsap/gsap.js"></script>
+<script src="assets/vendors/gsap/scroll-smoother.js"></script>
+<script src="assets/vendors/gsap/scroll-trigger.js"></script>
+<script src="assets/vendors/gsap/scroll-to-plugin.js"></script>
+<script src="assets/vendors/gsap/split-text.js"></script>
+<script src="assets/vendors/gsap/gsap-lotech.js"></script>
+<!-- template js -->
+<script src="assets/js/lotech.js"></script>
 </body>
 
 
